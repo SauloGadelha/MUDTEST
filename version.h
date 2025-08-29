f2acc4d9baaa3e34493e41362f7c841c273e9694
SauloGadelha <saulogadelhaadvogado@gmail.com>
2025-08-29 17:39:20 -0300

    Add files via upload

---
+++ version.h
@@ -0,0 +1,8 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+static const char * BuildVersion = "";
+static const char * BuildTime = "Wed Aug 13 10:09:17 -03 2025";
+static const char * BuildPath = "/cygdrive/c/Users/Pc/Desktop/NP/AVENDAR/avendar/trunk";
+
+#endif
